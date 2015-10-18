3c976e347773d035310aa47da9ad6482308796bf
あまみや ゆうこ <1@1234.sh>
2015-10-18 21:51:11 +0800

    Init

---
+++ samples/CodeComb.Net.EmailSender.Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeComb.Net.EmailSender.Sample
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var mail = new SmtpEmailSender("smtp.qq.com", 25, "Code Comb", "YourQQNumber@qq.com", "YourQQNumber", "YourPassword");
+            mail.SendEmailAsync("1@1234.sh","关于MailSender项目说明", "<html><head><meta http-equiv=\"Content-type\" content=\"text/html;charset=utf-8\"/></head><body>能收到邮件吗</body></html>").Wait();
+        }
+    }
+}
---
+++ src/CodeComb.Net.EmailSender/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CodeComb.Net.EmailSender")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodeComb.Net.EmailSender")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cd84d240-c19b-460e-90e1-0217fc56454a")]
